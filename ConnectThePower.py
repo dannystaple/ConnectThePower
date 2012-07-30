11cf72fd219164fbb427a72333f65da6899d8e64
stapled <danny@orionrobots.co.uk>
2012-07-30 20:58:36 +0100

    the game now gets the right coords for cells. Start with separation of gameplay and ui.

--- ConnectThePower.py
+++ ConnectThePower.py
@@ -25,7 +25,27 @@ class Splash(object):
     """Picture of crazy electrician. Clicks/presses fade to menu"""
     pass
 
-class MainGame(SceneBase):
+class GameCore(object):
+    grid_cells = 5
+
+    STRAIGHT_SEGMENT = object()
+
+    def __init__(self):
+        self.grid = {}
+
+    def playMove(self, coords, move, rotation):
+        self.grid[coords] = (move, rotation)
+
+    def getMove(self, coords):
+        return self.grid[coords]
+
+    def allMoves(self):
+        return [(coords, item[0], item[1]) for coords, item in self.grid.items()]
+
+    def nextSegment(self):
+        return self.STRAIGHT_SEGMENT
+
+class MainGameUI(SceneBase):
     electric_blue = (192, 192, 255)
     black = (0, 0, 0)
     grey = (0x88, 0x88, 0x88)
@@ -35,40 +55,48 @@ class MainGame(SceneBase):
     top_coord = 50
 
     game_rect_colour = black
-    game_rect_height = 300
-    game_rect_width = 300
-    game_rect_left = 170
-    game_rect = (game_rect_left, top_coord, game_rect_width, game_rect_height)
+    game_rect = pygame.Rect(170, top_coord, 300, 300)
 
-    straight_line = pygame.image.load("StraightLine.png")
+    segments = {GameCore.STRAIGHT_SEGMENT: pygame.image.load("StraightLine.png")}
 
     next_item_rect = (35, top_coord, 100, 100)
     next_item_colour = black
 
     grid_size = 50
     grid_colour = grey
-    grid_left = game_rect_left
-    grid_right = game_rect_left + game_rect_width
-    grid_top = top_coord
-    grid_bottom = top_coord + game_rect_height
-
-
+    grid_left = game_rect.left
+    grid_right = game_rect.right
+    grid_top = game_rect.top
+    grid_bottom = game_rect.bottom
+    grid_cols = tuple([col for col in range(grid_left, grid_right, grid_size)])
+    grid_rows = tuple([row for row in range(grid_top, grid_bottom, grid_size)])
 
     def enter(self):
         """Enter the game screen"""
+        self._core = GameCore()
+
         self._playing = True
 
         while self._playing:
             event = pygame.event.poll()
             if event.type == pygame.QUIT:
                 self._playing = False
+            elif event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                self._playMove(pos)
             self._render()
 
     def _draw_grid(self):
-        [self._line(self.grid_colour, (self.grid_left, row + self.top_coord), (self.grid_right, row + self.top_coord))
-         for row in range(0, self.game_rect_height, self.grid_size)]
-        [self._line(self.grid_colour, (self.grid_left + col, self.grid_top), (self.grid_left + col, self.grid_bottom))
-         for col in range(0, self.game_rect_width, self.grid_size)]
+        [self._line(self.grid_colour, (self.grid_left, row), (self.grid_right, row))
+         for row in self.grid_rows]
+        [self._line(self.grid_colour, (col, self.grid_top), (col, self.grid_bottom))
+         for col in self.grid_cols]
+
+    def _renderMoves(self):
+        for coords, move, rotation in self._core.allMoves():
+            segment = self.segments[move]
+            coords = self.fromCoreGrid(coords)
+            self._screen.blit(segment, pygame.Rect(coords, (self.grid_size, self.grid_size)))
 
     def _render(self):
         self._screen.fill(self.bg_colour)
@@ -77,10 +105,34 @@ class MainGame(SceneBase):
 
         self._draw_grid()
 
-        self._screen.blit(self.straight_line, self.next_item_rect)
+        self._screen.blit(self.segments[self._core.nextSegment()], self.next_item_rect)
 
+        self._renderMoves()
         pygame.display.flip()
 
+    def toCoreGrid(self, pos):
+        """Given a position, convert to game core grid coords"""
+        x,y = pos
+        x -= self.game_rect.left
+        y -= self.game_rect.top
+        x /= self.grid_size
+        y /= self.grid_size
+        return x, y
+
+    def fromCoreGrid(self, pos):
+        """Given a core grid coord, convert to ui position"""
+        x, y= pos
+        x *= self.grid_size
+        y *= self.grid_size
+        x += self.game_rect.left
+        y += self.game_rect.top
+        return x, y
+
+    def _playMove(self, pos):
+        if self.game_rect.collidepoint(pos):
+            print "Played in rect"
+            pos = self.toCoreGrid(pos)
+            self._core.playMove(pos, self._core.nextSegment(), 0)
 
 class MainMenu(object):
     """Game, settings, exit"""
@@ -97,7 +149,7 @@ def init():
 def main():
     screen = init()
     spInstance = Splash()
-    game = MainGame(screen)
+    game = MainGameUI(screen)
     menu = MainMenu()
     settings = Settings()
     game.enter()    
