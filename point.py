f563e4987936cf23dd0b24c3f9f10a8f3d2c2d24
Danny Staple <danny@orionrobots.co.uk>
2022-07-22 13:52:38 +0100

    Fix the first point test

--- point.py
+++ point.py
@@ -30,11 +30,14 @@ class Point(object):
     def __hash__(self):
         return hash((self.x, self.y))
 
-    def __cmp__(self, other):
-        if (self.x == other.x) and (self.y == other.y):
-            return 0
-        else:
-            return 1
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    # def __cmp__(self, other):
+    #     if (self.x == other.x) and (self.y == other.y):
+    #         return 0
+    #     else:
+    #         return 1
 
     def __getitem__(self, item):
         if item is 0:
